fbb8d2b8e53216316d6f26dfa3816366c7950a3b
Lj Wright <Lauren.wright12136@gmail.com>
2025-08-28 15:43:26 -0400

    Lab 1 - Personal Introduction
    Lauren Wright - Personal introduction.

---
+++ Lab1/Lab1.py
@@ -0,0 +1,12 @@
+#Lab one - Personal Introduction
+print("My name is Lj. (Train/Wreck)")
+print('I am taking this class as part of the program I am pursuing.')
+print('I am pursuing an associates in Computer Information Systems'
+      'Software Engineering Option – Full Stack Development Focus.')
+print('When I am not in class I spend my free time being a massive'
+      ' nerd and working on my art work.')
+print('I have a small art studio set up in my apartment.'
+      'I produce art under "The Trainwreck Art Saloon".')
+print('I am a huge DnD girl as well as an avid Magic player.'
+      'I may not be the best but I have tons of fun playing and being the problem at the table')
+print('Fun fact about me: I love EDM and attend Lost Lands every year. ')
